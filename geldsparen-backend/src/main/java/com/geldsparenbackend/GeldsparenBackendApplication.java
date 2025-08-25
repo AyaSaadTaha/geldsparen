c2356eaaf19ab241a59166d33dde90c933503664
Aya Kamel <ayasaadtaha@gmail.com>
2025-08-25 13:06:33 +0200

    Projekt backend und frontend model erstellt!

---
+++ geldsparen-backend/src/main/java/com/geldsparenbackend/GeldsparenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.geldsparenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeldsparenBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeldsparenBackendApplication.class, args);
+    }
+
+}
---
+++ geldsparen-backend/src/test/java/com/geldsparenbackend/GeldsparenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geldsparenbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeldsparenBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
