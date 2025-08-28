2c7846be6fbdb43e86710512b90fe197fdd93aff
Aya Kamel <ayasaadtaha@gmail.com>
2025-08-28 09:47:52 +0200

    frontend edit!

--- geldsparen-frontend/src/components/auth/AuthForm.jsx
+++ geldsparen-frontend/src/components/auth/AuthForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/authHooks.js';
 import './AuthForm.css';
 import { useNavigate } from 'react-router-dom';
 
--- geldsparen-frontend/src/context/AuthContext.jsx
+++ geldsparen-frontend/src/context/AuthContext.jsx
@@ -1,16 +1,7 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import { authAPI } from '../services/auth';
 
 const AuthContext = createContext();
-
-// eslint-disable-next-line react-refresh/only-export-components
-export const useAuth = () => {
-    const context = useContext(AuthContext);
-    if (!context) {
-        throw new Error('useAuth must be used within an AuthProvider');
-    }
-    return context;
-};
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')) || null);
